b18cc7fe869c71b0a5e4154b50c111d38025a3c0
cwjworld <564521556@qq.com>
2022-01-29 20:16:24 +0800

    函数的递归和迭代

---
+++ Function.c/Function.c/function6.c
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+#include <string.h>
+//模拟实现一个strlen函数
+//方法一(创建了临时变量)
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//
+//	printf("%d\n", my_strlen(arr));
+//
+//	return 0;
+//}
+//
+////使用递归的方法(不允许创建临时变量,即没有count)
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	printf("%d\n", my_strlen(arr));
+//	return;
+//}
+//
+////递归与迭代
+////求n的阶乘,不考虑溢出
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int ret = 1;
+//	//循环是一种迭代的方式
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//int Fac(int n)
+//{
+//	if (n <= 1)
+//		return 1; 
+//	else
+//		return n * Fac(n - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Fac(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//求第n个斐波那契数
+//递归可以求解，但是效率太低
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	if (n > 2)
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Fib(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+int Fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int ret = Fib(n);
+	printf("%d\n", ret);
+	return 0;
+}
