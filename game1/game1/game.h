2e84c2e7611374d3f9a132c0cacc6a4f7d7702c5
cwjworld <564521556@qq.com>
2022-02-04 22:27:31 +0800

    三子棋完整版

--- game1/game1/game.h
+++ game1/game1/game.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 //符号的定义
 #define ROW 3	
 #define COL 3
@@ -10,4 +12,12 @@ void InitBoard(char board[ROW][COL], int row, int col);
 //打印棋盘的函数
 void DisplayBoard(char board[ROW][COL], int row, int col);
 
+//玩家下棋
+void PlayerMove(char board[ROW][COL], int row, int col);
+
+//电脑下棋
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+//判断游戏是否有输赢
+char IsWin(char board[ROW][COL], int row, int col);
 
--- game1/game1/test.c
+++ game1/game1/test.c
@@ -16,10 +16,44 @@ void game()
 	InitBoard(board, ROW, COL);
 	//打印棋盘 - 本质是打印数组的内容
 	DisplayBoard(board, ROW, COL);
+	char ret = 0;
+	//玩家走
+	//电脑走
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断玩家是否赢得游戏
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+			break;
+		//电脑下棋
+		//判断电脑是否赢得游戏
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+			break;
+	}
+		if (ret == '*')
+		{
+			printf("玩家赢了\n");
+		}
+		else if (ret == '#')
+		{
+			printf("电脑赢了\n");
+		}
+		else
+		{
+			printf("平局\n");
+		}
+	DisplayBoard(board, ROW, COL);
 }
 int main()
 {
 	int input = 0;
+	srand((unsigned int)time(NULL));
 	do
 	{
 		menu();
