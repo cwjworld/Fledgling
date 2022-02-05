cd710f5c7e8864b99d2c0ab755906183cd5ece0d
cwjworld <564521556@qq.com>
2022-02-05 23:57:20 +0800

    扫雷1

---
+++ game2/game2/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+void menu()
+{
+	printf("******************************");
+	printf("********   1.play    *********");
+	printf("********   0.exit    *********");
+	printf("******************************");
+}
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("ÇëÑ¡Ôñ:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+
+		}
+	} while ();
+	return 0;
+}
