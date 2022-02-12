2ebc4bb2113c1175fe5ca5c0474885862da84954
cwjworld <564521556@qq.com>
2022-02-12 23:46:36 +0800

    表达式求值

---
+++ Operator/Operator/Operator4.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	char a = 3;
+	char b = 127;
+	char c = a + b;
+	printf("%d\n", c);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
