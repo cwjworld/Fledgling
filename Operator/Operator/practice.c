ebdae77c42d0be8a323a6adcec8a029e6cbc8e33
cwjworld <564521556@qq.com>
2022-02-15 21:16:29 +0800

    操作符练习

--- Operator/Operator/practice.c
+++ Operator/Operator/practice.c
@@ -1,1 +1,84 @@
-#define _CRT_SECURE_NO_WARNINGS 1 
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+#include <string.h>
+
+int main()
+{
+	int arr[] = { 1,2,(3,4),5 };
+	printf("%d\n", sizeof(arr));
+	return 0;
+}
+
+int main()
+{
+	char str[] = "hello bit";
+	printf("%d %d\n", sizeof(str), strlen(str));
+	return 0;
+}
+
+//实现函数init() 初始化数组全为0
+//实现print() 打印数组的每个元素
+//实现reverse() 函数完成数组元素的逆置
+void init(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		arr[i] = 0;
+	}
+}
+
+void print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+void reverse(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+
+	while (left < right)
+	{
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);
+	reverse(arr, sz);
+	print(arr, sz);
+	init(arr, sz);
+	print(arr, sz); 
+	return 0;
+}
+
+//将a,b数组内容交换，假设大小一样
+int main()
+{
+	int arr1[] = { 1,3,5,7,9 };
+	int arr2[] = { 2,4,6,8,10 };
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		int tmp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = tmp;
+	}
+	
+	return 0;
+}
