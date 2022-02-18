3f7e211cddb20615500a061a5c39ab3549bff41c
cwjworld <564521556@qq.com>
2022-02-18 21:47:37 +0800

    指针运算

--- Pointer/Pointer/Pointer2.c
+++ Pointer/Pointer/Pointer2.c
@@ -29,4 +29,29 @@ int main()
 
 //3.指针指向的空间释放
 
+//指针的运算
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;
+	int* pend = arr + 9;
+
+	while (p <= pend)
+	{
+		printf("%d\n", *p);
+		p++; 
+	}
+	return 0;
+}
+
+
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	printf("%d\n", &arr[9] - &arr[0]);
+	//指针减去指针得到的是两个指针之间的元素
+	//两个指针需要指向同一块空间
+	return 0;
+}
+
 
