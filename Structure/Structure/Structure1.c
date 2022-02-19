ca979d9c7335263cd56fe31bb9793e9bff6d4dca
cwjworld <564521556@qq.com>
2022-02-19 22:35:36 +0800

    结构体

---
+++ Structure/Structure/Structure1.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <stdio.h>
+
+struct B
+{
+	char c;
+	short s;
+	double d;
+};
+struct Stu
+{
+	struct B sb;
+	char name[20];
+	int age;
+	char id[20];
+}; s1, s2;//s1,s2也是两个结构体变量
+//s1,s2是全局变量
+int main()
+{
+	//s是局部变量
+	struct Stu s = { {'w', 20, 3.14},"张三", 30, 202005034};//对象
+	
+	return 0;
+}
