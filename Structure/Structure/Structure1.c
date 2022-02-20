4ed8622e087aac658b0b48984dba02176c7f2a79
cwjworld <564521556@qq.com>
2022-02-20 20:35:13 +0800

    结构体初阶

--- Structure/Structure/Structure1.c
+++ Structure/Structure/Structure1.c
@@ -20,6 +20,9 @@ int main()
 {
 	//s是局部变量
 	struct Stu s = { {'w', 20, 3.14},"张三", 30, 202005034};//对象
-	
+	//.和->结构体成员访问操作符
+	struct Stu* ps = &s;
+	printf("%c\n", s.sb.c);
+	printf("%c\n", ps->sb.c);
 	return 0;
 }
