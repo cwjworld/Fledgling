41f25ba9deadc8b8f30df97fe427a81e6b0d4e62
cwjworld <564521556@qq.com>
2022-02-28 22:46:58 +0800

    数据储存

---
+++ Storage/Release/Storage.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+
+#include <stdio.h>
+
+//整形的存储
+int main()
+{
+	int a = -10;
+	int b = 10;
+	return 0;
+}
+
+//大端字节序和小端字节序
+int main()
+{
+	int a = 0x11223344;
+
+	return 0;
+}
