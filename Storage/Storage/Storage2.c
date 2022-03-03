8f479f1235dc5087c2e9e3a874b44f6aa495df77
cwjworld <564521556@qq.com>
2022-03-03 22:49:30 +0800

    浮点型存储

--- Storage/Storage/Storage2.c
+++ Storage/Storage/Storage2.c
@@ -2,8 +2,32 @@
 
 #include <stdio.h>
 
+#include <string.h>
+
 int main()
 {
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	printf("%d", strlen(a));
+	return 0;
+}
+
+//浮点型在内存中的存储
+
+int main()
+{
+	int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为: %d\n", n);
+	printf("*pFloat的值为: %f\n", *pFloat);
+
+	*pFloat = 9.0;
+	printf("num的值为: %d\n", n);
+	printf("*pFlaot的值为: %f\n", *pFloat);
 
 	return 0;
 }
